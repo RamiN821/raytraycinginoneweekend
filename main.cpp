40320a960a1b13a4b7a86aa4803c1e024b826bea
Rami Niskanen <rami.niskanen@gmail.com>
2026-06-17 16:39:07 +0300

    Added camera

--- main.cpp
+++ main.cpp
@@ -4,16 +4,43 @@
 
 #include <iostream>
 
+colour ray_colour(const ray& r){
+  return colour(0,0,0);
+}
+
 int main() {
-  int image_width = 256;
-  int image_height = 256;
+  auto aspect_ratio = 16.0 / 9.0;
+  int image_width = 400;
+
+  int image_height = int(image_width / aspect_ratio);
+  image_height = (image_height > 1) ? 1 : image_height;
+
+  //Camera
+
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (double(image_width) / image_height);
+  auto camera_center = point3(0, 0, 0);
+
+  auto viewport_u = vec3(viewport_width, 0, 0);
+  auto viewport_v = vec3(0, viewport_height, 0);
+
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixe_delta_v = viewport_v / image_height;
+
+  auto viewpoer_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+  auto pixel00_loc = viewpoer_upper_left + 0.5 * (pixel_delta_u + pixe_delta_v);
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++){
     std::clog << "\rScanlines remaining: " << (image_height - j) << std::flush;
     for (int i = 0; i < image_width; i++){
-      auto pixel_colour = colour(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixe_delta_v);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
+
+      colour pixel_colour = ray_colour(r);
       write_colour(std::cout, pixel_colour);
     }
   }
