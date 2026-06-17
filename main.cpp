a0b3c444fce726346fed226a6022c6650eb9f1cd
Rami Niskanen <rami.niskanen@gmail.com>
2026-06-17 20:44:40 +0300

    Added sphere collider

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, const double& radius, const ray& r){
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2.0 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant >= 0);
+}
+
 colour ray_colour(const ray& r){
-  return colour(0,0,0);
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)){
+    return colour(1,0,0);
+  }
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-a)*colour(1.0,1.0,1.0) + a*colour(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -13,7 +27,7 @@ int main() {
   int image_width = 400;
 
   int image_height = int(image_width / aspect_ratio);
-  image_height = (image_height > 1) ? 1 : image_height;
+  image_height = (image_height < 1) ? 1 : image_height;
 
   //Camera
 
